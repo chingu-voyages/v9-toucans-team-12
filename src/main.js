409c9f62fca55878400e93b4246365c1352b9f4b
Ashiya1005 <ashiya1005@gmail.com>
2019-06-26 16:00:19 +1000

    Updated search when all is clicked

--- src/main.js
+++ src/main.js
@@ -1,16 +1,19 @@
-$('.donated-item-menu li').click(function(){
-  $('.donated-item-menu li').removeClass('active');
-  $(this).addClass('active');
-  var currentTag = $(this).attr('data-filter');
 
-  $('.donated-item-container div').each(function(){
-    if ($(this).hasClass(currentTag)) {
-      $(this).show();
-      $(this).fadeIn();
-    }
-    else {
-      $(this).hide();
-    }
-  });
+//************ Find form - search donated items ***************//
+$('.donated-item-tag li').click(function(){
+  //Change the donated menu class
+  $('.donated-item-tag li').removeClass('active');
+  $(this).addClass('active');
+  //Get the current tag
+  var currentTag = $(this).attr('data-filter');
+  //Display all items if tag All is clicked
+  if(currentTag === '*') {
+    $('.donated-item').show('1000');
+  }
+  else {
+    //Display only items whose tag is active and transition effect
+    $('.donated-item').not('.'+currentTag).hide('5000');
+    $('.donated-item').filter('.'+currentTag).show('5000');
+  }
 
 });
